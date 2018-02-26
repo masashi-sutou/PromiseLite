705630ed445d82c9bbfd5d6e512b069f0342a6e1
Masashi Sutou <sutou.masasi@gmail.com>
2018-02-26 23:37:57 +0900

    Initial Commit

---
+++ PromiseLite/PromiseLite.h
@@ -0,0 +1,19 @@
+//
+//  PromiseLite.h
+//  PromiseLite
+//
+//  Created by 須藤将史 on 2018/02/26.
+//  Copyright © 2018年 須藤将史. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PromiseLite.
+FOUNDATION_EXPORT double PromiseLiteVersionNumber;
+
+//! Project version string for PromiseLite.
+FOUNDATION_EXPORT const unsigned char PromiseLiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PromiseLite/PublicHeader.h>
+
+
